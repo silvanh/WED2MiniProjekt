1be1e1402f97b1b3ff33afa9528ec2c41833a080
shabegge <shabegge@hsr.ch>
2015-10-23 09:37:15 +0200

    Controllertests mit jasmine hinzugefügt

--- App/tests/classes/controllers/eventListControllerTest.js
+++ App/tests/classes/controllers/eventListControllerTest.js
@@ -1,3 +1,19 @@
 /**
  * Created by silvan on 10/21/15.
  */
+
+
+define(['app/controllers/eventListController'], function (EventListController) {
+    'use strict';
+
+    describe('EventListController', function() {
+        describe('property scope', function() {
+            it('contains 3 events', function() {
+                var scope = {};
+                var eventListController = new EventListController(scope);
+
+                expect(3).toBe(eventListController.scope.events.length);
+            });
+        });
+    });
+});
--- App/tests/classes/tests.js
+++ App/tests/classes/tests.js
@@ -1,3 +1,33 @@
 /**
  * Created by silvan on 10/21/15.
  */
+require.config({
+    baseUrl: './',
+    paths: {
+        'frameworks/angular': '../source/frameworks/angular/angular.min',
+        'app': '../source/classes',
+        'tests': 'classes',
+        'libraries/jasmine': ['libraries/jasmine/jasmine'],
+        'libraries/jasmine-html': ['libraries/jasmine/jasmine-html'],
+        'libraries/jasmine-boot': ['libraries/jasmine/boot']
+    },
+    shim: {
+        'frameworks/angular': {
+            exports: ['angular']
+        },
+        'libraries/jasmine-html': {
+            deps : ['libraries/jasmine']
+        },
+        'libraries/jasmine-boot': {
+            deps : ['libraries/jasmine', 'libraries/jasmine-html']
+        }
+    }
+});
+
+
+require(['libraries/jasmine-boot'], function () {
+    require(['tests/controllers/eventListControllerTest'], function(){
+        //trigger Jasmine
+        window.onload();
+    })
+});
