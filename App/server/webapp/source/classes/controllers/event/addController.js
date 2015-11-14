9e37847294b1f219161afac51345b0bdb9b60638
shabegge <shabegge@hsr.ch>
2015-11-14 16:28:49 +0100

    add datepicker

--- App/server/webapp/source/classes/controllers/event/addController.js
+++ App/server/webapp/source/classes/controllers/event/addController.js
@@ -1,9 +1,42 @@
-define([], function() {
+define(['app/model/event'], function(Event) {
   'use strict';
 
-  var EventAddController = function($scope, $routeParams, EventRepository) {
+  var EventAddController = function($scope, $routeParams, EventRepository,event) {
     this.scope = $scope;
-    this.scope.test = "just a test site";
+
+    $scope.clearStartDate = function () {
+      $scope.startDate = null;
+    };
+
+    $scope.clearEndDate = function () {
+      $scope.endDate = null;
+    };
+
+    $scope.openStartDate = function($event) {
+      $scope.status.startDateOpened = true;
+    };
+
+    $scope.openEndDate = function($event) {
+      $scope.status.endDateOpened = true;
+    };
+
+    $scope.setStartDate = function(year, month, day) {
+      $scope.startDate = new Date(year, month, day);
+    };
+
+    $scope.setEndDate = function(year, month, day) {
+      $scope.endDate = new Date(year, month, day);
+    };
+
+    $scope.format = 'dd-MM-yy';
+    $scope.status = {
+      startDateOpened: false,
+      endDateOpened: false
+    };
+
+    this.scope.add =  function(){
+      var newEvent = new Event();
+    }
   }
 
   return EventAddController;
