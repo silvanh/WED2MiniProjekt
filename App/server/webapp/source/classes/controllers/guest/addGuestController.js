b6e94fc31632bc21ebada32f2bb13313b0317f53
Manuel Roth <manuelroth@hotmail.ch>
2015-11-25 14:27:51 +0100

    Implement addGuestController

--- App/server/webapp/source/classes/controllers/guest/addGuestController.js
+++ App/server/webapp/source/classes/controllers/guest/addGuestController.js
@@ -1,9 +1,26 @@
 define(['app/model/guest'], function(Guest) {
   'use strict';
 
-  var GuestAddController = function($scope,$routeParams) {
-    this.scope=$scope;
-    this.scope.test="test"
+  var GuestAddController = function($scope, $routeParams, GuestRepository, $location) {
+    this.scope = $scope;
+
+    this.scope.addGuest =  function(){
+      if( $scope.guestName && $scope.guestContribution && $scope.guestComment) {
+      	$scope.addGuestError = '';
+      	var newGuest = new Guest($scope.guestName, $scope.guestContribution, $scope.guestComment, false);
+      	GuestRepository.add(
+      		$routeParams.eventId,
+        	newGuest,
+        	function(guest) {
+        	  $location.path('/events/' + $routeParams.eventId);
+        	},
+        	function() {}
+      	);
+      } else {
+      	$scope.addGuestError = "Please fill out all fields";
+      }
+      
+    }
   }
   return GuestAddController;
 });
